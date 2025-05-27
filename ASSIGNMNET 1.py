59f79d03af09e4f1ac11ff3877eb6862f58305c3
r070805 <rebeccabobby07@gmail.com>
2025-05-27 13:11:17 +0530

    Add files via upload

---
+++ ASSIGNMNET 1.py
@@ -0,0 +1,11 @@
+#task 1
+a=int(input("enter number 1"))
+b=int(input("enter number 2"))
+print("addition:",a+b)
+print("subtaraction:",a-b)
+print("multiplication:",a*b)
+print("division:",a/b)
+#task 2
+c=input("enter first name:")
+d=input("enter last name:")
+print("Hello,",c,d,"! Welcome to the Python program.")
